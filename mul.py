e3350419fd3340fce517db4ddc26f5c2bbfeef0f
Joo Hyun Lee <112560299+gongkeo@users.noreply.github.com>
2024-06-05 16:42:01 +0900

    오탈자 수정

--- mul.py
+++ mul.py
@@ -60,7 +60,7 @@ def detect_multiple(path, num_list, total_qna_num, reader):
                 # 체크한 선지 번호 check 감지
                 else:
                     check = labelToInt(names[int(cls)])
-                    if not (check in checklist):
+                    if not (check in check_list):
                         check_list.append(check)           
             
             if (len(check_list) > 1):
